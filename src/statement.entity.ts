fbd15816d6b755915f1c7999832de8b131349dd1
cristovao <cristovao>
2026-08-13 19:52:08 +0100

    faet: update statemet entity

--- src/statement.entity.ts
+++ src/statement.entity.ts
@@ -30,6 +30,4 @@ export class Statement extends entity<statementProps>{
             }
         }
     }
-
-    
 }
--- src/statementmemory.repo.ts
+++ src/statementmemory.repo.ts
@@ -1,15 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { IstatementRepo } from "./Istatement.repo";
+import { Statement } from "./statement.entity";
 
 Injectable()
 export class statementRepository implements IstatementRepo{
-    private _statements: any[];
+    private _statements: Statement[];
     constructor(){
         this._statements = [];
     }
     
     public async create(document: string, credit: string, value: number): Promise<void> {
-            await this._statements.push({document, credit, value});
+            
+        const newStatement = Statement.create(document, credit, value)
+            await this._statements.push(newStatement);
     }
 
     public async findAll(): Promise<any[]> {       
