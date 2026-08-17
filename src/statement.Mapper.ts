4d25bc6283943020be585158a634fed139f7c070
WanyCris <nesselmocristovao@gmail.com>
2026-08-17 08:14:44 -0300

    feat: add statement mapper

--- src/statement.Mapper.ts
+++ src/statement.Mapper.ts
@@ -1,14 +1,27 @@
+import { statement } from "@prisma/client";
 import { Statement } from "./statement.entity";
 
 export class statementMapper{
 
-    toEntity(){}
+    toEntity(rawData:statement){
+        return Statement.create
+        (
+         rawData.document,
+         rawData.creditcard,
+         rawData.value,
+         rawData.uuid   
+        )
+    }
 
     toTable(domianEntity:Statement){
         
         return{
+
             uuid:domianEntity.UUId,
-            document:domianEntity.
+            document:domianEntity.document,
+            creditcard:domianEntity.credit,
+            valeu:domianEntity.value
+
         }
     }
 }
