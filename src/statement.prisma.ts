63f5d65d3601f7396e1a32cda707af514892cca7
cristovao <cristovao>
2026-08-17 23:46:53 +0100

    feat: add prisma repo

--- src/statement.prisma.ts
+++ src/statement.prisma.ts
@@ -1,17 +1,34 @@
 import { IstatementRepo } from "./Istatement.repo"
 import { Statement } from "./statement.entity";
 import { PrismaService } from "./prisma/prisma.service";
+import { statementMapper } from "./statement.Mapper";
 
 
 export class statementPrisma implements IstatementRepo{
+
+    constructor(private readonly prismaService: PrismaService){}
     
-    create(newStatement: Statement): Promise<void> {
+    public async create(newStatement: Statement): Promise<void> {
         
-        const entityToTable = 
+        const entityToTable = statementMapper.toTable(newStatement);
+
+        await this.prismaService.statement.create({
+            data:{
+
+                uuid: entityToTable.uuid,
+                document: entityToTable.document,
+                creditcard: entityToTable.creditcard,
+                value: entityToTable.valeu
+            }
+        })
+        
         
     }
 
-    findAll(): Promise<any[]> {
-        
+    public async findAll(): Promise<any[]> {
+
+        const rawStatements = await this.prismaService.statement.findMany();
+        const statements = rawStatements.map( statement => statementMapper.toEntity(statement));
+        return statements;
     }
 }
